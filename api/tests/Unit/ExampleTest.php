f5684ce9ad7588b6a2e26e8a5a5e76e26a444660
Alvin Espejo <espejo.a2@gmail.com>
2025-07-06 17:49:54 +0800

    Added socialite package

--- api/tests/Unit/ExampleTest.php
+++ api/tests/Unit/ExampleTest.php
@@ -2,6 +2,9 @@
 
 namespace Tests\Unit;
 
+use App\Models\User;
+use Laravel\Socialite\Facades\Socialite;
+use Mockery;
 use PHPUnit\Framework\TestCase;
 
 class ExampleTest extends TestCase
@@ -13,4 +16,22 @@ class ExampleTest extends TestCase
     {
         $this->assertTrue(true);
     }
+
+    // public function test_socialite_login_callback()
+    // {
+    //     $abstractUser = Mockery::mock(\Laravel\Socialite\Two\User::class);
+    //     $abstractUser->shouldReceive('getId')->andReturn('123456');
+    //     $abstractUser->shouldReceive('getEmail')->andReturn('test@test.com');
+    //     $abstractUser->shouldReceive('getName')->andReturn('John Doe');
+    //     $abstractUser->shouldReceive('getAvatar')->andReturn('https://en.gravatar.com/userimage');
+
+    //     $provider = Mockery::mock(\Laravel\Socialite\Contracts\Provider::class);
+    //     $provider->shouldReceive('user')->andReturn($abstractUser);
+
+    //     Socialite::shouldReceive('driver')->with('facebook')->andReturn($provider);
+
+    //     // $this->visit(route("authFacebookCallback"))
+    //     //  ->seePageIs(route("home"));
+
+    // }
 }
